2715792908089c5dfd8a692bb94ab6651ae99988
Natan de Paula Rocha <natan.paularocha@gmail.com>
2025-12-13 19:43:03 -0300

    (chore): Corrections and adjustments in dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { routes } from './app.routes';
-import { provideRouter } from '@angular/router';
-
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import Lara from '@primeuix/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Lara,
+        options: {
+          darkModeSelector: false
+        }
       }
     })
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TicketListComponent } from './features/tickets/pages/ticket-list/ticket-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TicketListComponent }
+];
